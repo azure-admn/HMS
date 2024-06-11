4e9d46870d3ff83fe277ede8de755c5863277bc7
azure-admn <rajthipak5@gmil.com>
2024-06-11 22:31:44 +0530

    update commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/custom.global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Component {...pageProps} />
+  </div>
 }
-
 export default MyApp
